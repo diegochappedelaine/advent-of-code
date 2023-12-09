c15b545311e5eeb8f03fab70d4221b85eabda0fa
Diego Chappedelaine <chappedelaine.diego@gmail.com>
2023-12-09 13:44:31 +0100

    clean useless exports

--- years/2022/day-4/index.ts
+++ years/2022/day-4/index.ts
@@ -1,8 +1,8 @@
 import { result as resultPart1 } from "./part-1.ts";
 import { result as resultPart2 } from "./part-2.ts";
 
-export const firstResult = `Part 1: ${resultPart1}`;
-export const secondResult = `Part 2: ${resultPart2}`;
+const firstResult = `Part 1: ${resultPart1}`;
+const secondResult = `Part 2: ${resultPart2}`;
 
 console.log(firstResult);
 console.log(secondResult);
